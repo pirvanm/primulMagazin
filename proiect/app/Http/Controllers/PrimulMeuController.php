74d7d4547ffa0546d5f8ec0c4e1dc29f0a0eeb02
Pirvan Marian <pirvan.marian@gmail.com>
2021-03-11 18:14:54 +0200

    #14 update controller

--- proiect/app/Http/Controllers/PrimulMeuController.php
+++ proiect/app/Http/Controllers/PrimulMeuController.php
@@ -4,13 +4,32 @@ namespace App\Http\Controllers;
 // Include o clasa definit ca si model
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PrimulMeuController extends Controller
 {
     public function index()
     {
-
+        dd('sunt in metoda');
         return 'metoda din controler';
+    }
+
+    public function getData()
+    {
+        //  dd('metoda get data');
+        // $email = DB::table('users')
+        //     ->first();
+
+        $users = DB::table('users')
+            ->get();
+
+        // First() este o metoda care ia doar o intreare , ultima 
+        // get() ia toate datele din baza de date 
+
+        return $users;
+        dd($email);
+        // ->where('name', 'John')
+        //->value('email');
     }
 
     public function getHTML()
