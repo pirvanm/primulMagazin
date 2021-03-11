72f8c60f4206e90dfe964a788362ca25fa77a912
Pirvan Marian <pirvan.marian@gmail.com>
2021-03-11 18:15:23 +0200

    #15 toate migrarile

---
+++ proiect/database/migrations/2021_03_02_180316_persoane.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Persoane extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('persoane', function (Blueprint $table) {
+            //Corpul pentru coloane 
+            $table->bigIncrements('id');
+            //bigImcrement este tipul coloane
+            //   bigIncremenets('coloana');
+            $table->string('nume');
+            $table->string('prenume')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('telefon');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
